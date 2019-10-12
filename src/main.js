e4d228223065bb194c0b4d937640f05631142708
moming <2235868313@qq.com>
2019-10-12 11:43:31 +0800

    'vant插件的引入'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// =========全局引入vant 插件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+// =========
+
 Vue.config.productionTip = false
 
 new Vue({
