09dd4cb22e2ce68c4950dce908339499568e90c0
hhuuii97 <763961781@qq.com>
2019-10-14 20:59:57 +0800

    '登录界面'

--- src/login/login.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1>
-    登入界面、注册页面
-  </h1>
-
-</template>
--- src/views/cart/index.vue
+++ src/views/cart/index.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="fen-cart">
-    <p>购物车</p>
+    <van-nav-bar
+      title="购物车"
+      right-text="编辑"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
   </div>
 </template>
+<script>
+export default {
+  // methods: {
+  //   onClickLeft () {
+  //     Toast('返回')
+  //   },
+  //   onClickRight () {
+  //     Toast('按钮')
+  //   }
+  // }
+}
+</script>
