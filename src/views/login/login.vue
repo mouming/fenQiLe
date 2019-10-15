35cae9d77dbc2a637a8b1aad4713a4451d231a0c
hhuuii97 <763961781@qq.com>
2019-10-15 21:27:12 +0800

    'cart'

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,20 +2,9 @@
   <div class="fen-login">
     <section class="main-wrap">
       <div class="head-main">
-        <p>登录分期乐</p>
+        <p>{{logarr[logindex].logTitle}}</p>
       </div>
       <div class="login-banner"></div>
-      <!-- 手机号 -->
-      <!-- <inputGroutp
-        type="number"
-        v-model="phone"
-        placeholder="输入手机号"
-        :btnTitle="btnTitle"
-        :disabled="disabled"
-        :error="error.phone"
-      ></inputGroutp>-->
-      <!-- 6位数验证码 -->
-      <!-- <inputGroutp type="number" v-model="verifyCode" placeholder="6位数验证码" :error="error.code"></inputGroutp> -->
       <ul class="input-wrap">
         <li class="input-item">
           <i class="iconfont iconshouji"></i>
@@ -23,26 +12,30 @@
             class="input-set"
             id="test"
             type="text"
-            placeholder="输入手机号"
+            :placeholder="logarr[logindex].impTel"
             maxlength="13"
-            v-model="phone"
+            v-model="mobile"
+            ref="mobile"
+            @keyup="inputMobile"
+            @paste="inputMobile"
           />
         </li>
         <li class="input-item">
           <i class="iconfont iconverify"></i>
           <input
             class="input-set"
-            type="password"
+            type="text"
             placeholder="6位数字验证码"
             maxlength="6"
-            v-model="password"
+            ref="card"
+            v-model="card"
           />
-          <span @click="validateBtn">获取验证码</span>
+          <span @click="getYzm()">获取验证码</span>
         </li>
       </ul>
       <div class="btn-wrap">
-        <button :disabled="isValid" value="登录">
-          <!-- <span>登录</span> -->
+        <button>
+          <span>{{logarr[logindex].logNext}}</span>
         </button>
       </div>
       <p class="read-wrap">
@@ -50,47 +43,116 @@
         <router-link to="./registerprotocol">用户注册协议、</router-link>
         <router-link to="./privacyprotocol">隐私保护政策</router-link>
       </p>
-      <div class="link-box">没有账号？立即注册</div>
-      <div class="link-box">已有账号？立即登录</div>
+      <div class="link-box" @click="islog(logindex)">{{logarr[logindex].noAcc}}</div>
     </section>
   </div>
 </template>
 <script>
 export default {
   name: 'Login',
-  methods: {
-    getYzm() {
-      if (this.validatePhone()) {
-        // 发送验证码
-        this.validateBtn()
-      }
-    },
-    validateBtn() {
-      let time = 60
-      let timer = setInterval(() => {
-        if (time === 0) {
-          clearInterval(timer)
-          this.btnTitle = '获取验证码'
-          this.disabled = false
-        } else {
-          // 倒计时
-          this.btnTitle = time + '秒后重试'
-          this.disabled = true
-          time--
+  data() {
+    return {
+      mobile: '',
+      card: '',
+      logarr: [
+        {
+          logTitle: '登录分期乐',
+          impTel: '输入手机号',
+          logNext: '登录',
+          noAcc: '没有账号？立即注册'
+        },
+        {
+          logTitle: '注册分期乐账号',
+          impTel: '输入本人手机号以注册',
+          logNext: '下一步',
+          noAcc: '已有账号？立即登录'
         }
-      }, 1000)
+      ],
+      logindex: 0
+    }
+  },
+  methods: {
+    inputMobile(e) {
+      this.formatMobile(e)
+      this.mobile = this.$refs.mobile.value
     },
-    validatePhone() {
-      // 验证手机号码
-      if (!this.phone) {
-        return false
-      } else if (!/^1[345678]\d{9}$/.test(this.phone)) {
-        return false
-      } else {
-        return true
+    formatMobile(e) {
+      let val = this.$refs.mobile.value // 不可直接用this.mobile，第一方便提取该方法降低代码耦合度，第二直接用this.mobile,在输入汉字时按下shift按键会导致无法再输入和删除内容
+      let selStart = this.$refs.mobile.selectionStart // 选中区域左边界位置
+      let mobileLen = val.length
+      let value = this.getValue(e, val).substr(0, 11) // 获取输入/粘贴内容,并截取前11位
+      let len = value.length
+      if (len > 3 && len < 8) {
+        value = value.replace(/^(\d{3})/g, '$1 ')
+      } else if (len >= 8) {
+        value = value.replace(/^(\d{3})(\d{4})/g, '$1 $2 ')
       }
+      this.$refs.mobile.value = value
+      if (selStart !== mobileLen) {
+        if (selStart === 3) {
+          selStart++
+        }
+        // 设置光标位置
+        this.$refs.mobile.selectionStart = this.$refs.mobile.selectionEnd = selStart
+      }
+    },
+    getValue(e, val) {
+      let value = ''
+      if (e.type === 'keyup') {
+        value = val.replace(/\D/g, '')
+      } else if (e.type === 'paste') {
+        // window.clipboardData：IE浏览器获取剪贴板数据对象
+        // event.clipboardData：Chrome, Firefox, Safari获取剪贴板数据对象
+        let clipboardData = event.clipboardData || window.clipboardData
+        if (e.type.replace(/^1[345678]\d{9}$/)) {
+          value = clipboardData.getData('Text') // 获取剪贴板text格式的数据
+          value = value.replace(/\D/g, '')
+        } else {
+          value = ''
+        }
+      }
+      return value
+    },
+    getYzm() {
+      if ((this.mobile.length = 13)) {
+      }
+    },
+    islog() {
+      this.logindex = this.logindex ? 0 : 1
     }
   }
+  // methods: {
+  //   getYzm() {
+  //     if (this.phone()) {
+  //       // 发送验证码
+  //       this.validateBtn()
+  //     }
+  //   },
+  // validateBtn() {
+  //   let time = 60
+  //   let timer = setInterval(() => {
+  //     if (time === 0) {
+  //       clearInterval(timer)
+  //       this.btnTitle = '获取验证码'
+  //       this.disabled = false
+  //     } else {
+  //       // 倒计时
+  //       this.btnTitle = time + '秒后重试'
+  //       this.disabled = true
+  //       time--
+  //     }
+  //   }, 1000)
+  // },
+  // validatePhone() {
+  //   // 验证手机号码
+  //   if (!this.phone) {
+  //     return false
+  //   } else if (!/^1[345678]\d{9}$/.test(this.phone)) {
+  //     return false
+  //   } else {
+  //     return true
+  //   }
+  // }
 }
 </script>
 <style lang="scss">
